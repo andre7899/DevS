55207732ca21dc201e541c1b4e617cf968668e28
Andre <andre_hhh_032@hotmail.com>
2022-12-30 23:44:57 -0500

    ejercicios tema 4

--- OpenB/intro a la programacion/Ejercicios_tema_4/src/Main.java
+++ OpenB/intro a la programacion/Ejercicios_tema_4/src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Usando If
+        int numeroIf = 10;
+
+        if (numeroIf>0) System.out.println("El numero es positivo");
+        else if (numeroIf == 0) System.out.println("El numero es 0");
+        else System.out.println("El numero es negativo");
+        System.out.println();
+
+        //Usando bucle while
+        int numeroWhile = 0;
+        while (numeroWhile<3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+        System.out.println();
+
+
+        numeroWhile = 0;
+        //Usando Do while
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }while (numeroWhile <1);
+        System.out.println();
+
+
+        //Usando For
+        for (int numeroFor = 0;numeroFor <= 3;numeroFor++){
+            System.out.println(numeroFor);
+        }
+        System.out.println();
+
+
+        //Usando Switch
+        var estacion = "primavera";
+        switch (estacion){
+            case "verano":
+            case "invierno":
+            case "primavera":
+            case "otoño":
+                System.out.println("Estas en "+estacion);
+                break;
+            default:
+                System.out.println("Estacion inválida");
+        }
     }
 }
